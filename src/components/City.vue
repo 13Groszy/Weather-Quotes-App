5985e9cb7a78186cad02122295908008007926a9
Mateusz Daszkiewicz <topdrive@Mini-Mateusz.lan>
2022-02-10 21:04:52 +0000

    City name update in different component

--- src/components/City.vue
+++ src/components/City.vue
@@ -1,14 +1,12 @@
 <template>
     <div>
-
+        <h2>{{passedCity}}</h2>
     </div>
 </template>
 
 <script>
 export default {
     name: 'City',
-    props: {
-        city: String
-    },
+    props: ['passedCity']
 }
 </script>
